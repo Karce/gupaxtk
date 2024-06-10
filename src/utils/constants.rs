786432e2b552cc4ed07a9573cef8656773797af0
mostafaei2002 <sinamostafaei2002@gmail.com>
2024-06-10 18:28:11 +0330

    fix: update help texts

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -449,10 +449,10 @@ pub const XVB_MODE_MANUAL_DONATION_LEVEL: &str = "Set the XvB donation level man
 
 
 // Manual Donation Levels
-pub const XVB_DONATION_LEVEL_DONOR_HELP: &str = "To qualify you must be actively donating at least 1 kH/s (1hr and 24hr avg.)";
-pub const XVB_DONATION_LEVEL_VIP_DONOR_HELP: &str = "To qualify you must be actively donating at least 10 kH/s (1hr and 24hr avg.)";
-pub const XVB_DONATION_LEVEL_WHALE_DONOR_HELP: &str = "To qualify you must be actively donating at least 100 kH/s (1hr and 24hr avg.)";
-pub const XVB_DONATION_LEVEL_MEGA_DONOR_HELP: &str = "To qualify you must be actively donating at least 1000 kH/s (1hr and 24hr avg.)";
+pub const XVB_DONATION_LEVEL_DONOR_HELP: &str = "To qualify at least 1 kH/s will be actively donated (1hr and 24hr avg.)";
+pub const XVB_DONATION_LEVEL_VIP_DONOR_HELP: &str = "To qualify at least 10 kH/s will be actively donated (1hr and 24hr avg.)";
+pub const XVB_DONATION_LEVEL_WHALE_DONOR_HELP: &str = "To qualify at least 100 kH/s will be actively donated (1hr and 24hr avg.)";
+pub const XVB_DONATION_LEVEL_MEGA_DONOR_HELP: &str = "To qualify at least 1000 kH/s will be actively donated (1hr and 24hr avg.)";
 
 
 // CLI argument messages
