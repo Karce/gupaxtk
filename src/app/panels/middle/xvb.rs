b4ce7658b474d918af0fcb0ed47bc0b344bab41a
Cyrix126 <gupaxx@baermail.fr>
2024-11-02 18:24:23 +0100

    fix: typo

--- src/app/panels/middle/xvb.rs
+++ src/app/panels/middle/xvb.rs
@@ -130,7 +130,7 @@ impl crate::disk::state::Xvb {
 
         // --------------------------- XVB Simple -------------------------------------------
         if self.simple && ui.checkbox(&mut self.simple_hero_mode, "Hero Mode").on_hover_text(XVB_HERO_SELECT).clicked() {
-            // change rutime mode immediatly.
+            // change rutime mode immediately.
             if self.simple_hero_mode {
                 api.lock().unwrap().stats_priv.runtime_mode = RuntimeMode::Hero;
             }  else {
